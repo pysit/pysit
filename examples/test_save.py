91dfb19e6bd49738d88ccf159877539362ded4b8
lebrat <leo.lebrat@gmail.com>
2015-09-20 15:37:32 -0400

    Adding the save option to reload shots

---
+++ examples/test_save.py
@@ -0,0 +1,183 @@
+# test for generate seismic data
+# Std import block
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pysit import *
+from pysit.gallery import horizontal_reflector
+import os
+import shutil
+
+#   Define Domain
+pmlx = PML(0.1, 100)
+pmlz = PML(0.1, 100)
+os.environ["OMP_NUM_THREADS"] = "12"
+x_config = (0.1, 1.0, pmlx, pmlx)
+z_config = (0.1, 0.8, pmlz, pmlz)
+
+d = RectangularDomain(x_config, z_config)
+
+m = CartesianMesh(d, 91, 71)
+
+#   Generate true wave speed
+C, C0, m, d = horizontal_reflector(m)
+
+# Set up shots
+zmin = d.z.lbound
+zmax = d.z.rbound
+zpos = zmin + (1./9.)*zmax
+
+shots = equispaced_acquisition(m,
+                               RickerWavelet(10.0),
+                               sources=5,
+                               source_depth=zpos,
+                               source_kwargs={},
+                               receivers='max',
+                               receiver_depth=zpos,
+                               receiver_kwargs={},
+                               )
+
+shots_pickle = equispaced_acquisition(m,
+                               RickerWavelet(10.0),
+                               sources=5,
+                               source_depth=zpos,
+                               source_kwargs={},
+                               receivers='max',
+                               receiver_depth=zpos,                               
+                               receiver_kwargs={},
+                               )
+
+shots_savemat = equispaced_acquisition(m,
+                               RickerWavelet(10.0),
+                               sources=5,
+                               source_depth=zpos,
+                               source_kwargs={},
+                               receivers='max',
+                               receiver_depth=zpos,                               
+                               receiver_kwargs={},
+                               )
+
+shots_h5py = equispaced_acquisition(m,
+                               RickerWavelet(10.0),
+                               sources=5,
+                               source_depth=zpos,
+                               source_kwargs={},
+                               receivers='max',
+                               receiver_depth=zpos,                               
+                               receiver_kwargs={},
+                               )
+
+
+# Define and configure the wave solver
+trange = (0.0,3.0)
+
+solver_time = ConstantDensityAcousticWave(m,
+                                          spatial_accuracy_order=6,
+                                          kernel_implementation='cpp',
+                                          trange=trange)
+# Generate Seismic data/ Loading it
+
+print "shot TIME generation..."
+base_model = solver_time.ModelParameters(m,{'C':C})
+generate_seismic_data(shots, solver_time, base_model)
+print "DONE"
+
+print "pickle TIME generation..."
+generate_seismic_data(shots, solver_time, base_model,save_method='pickle')
+generate_seismic_data_from_file(shots_pickle, solver_time, save_method='pickle')
+print "DONE"
+
+print "savemat TIME generation..."
+generate_seismic_data(shots, solver_time, base_model,save_method='savemat')
+generate_seismic_data_from_file(shots_savemat, solver_time, save_method='savemat')
+print "DONE"
+print "h5py TIME generation..."
+generate_seismic_data(shots, solver_time, base_model,save_method='h5py')
+generate_seismic_data_from_file(shots_h5py, solver_time, save_method='h5py')
+print "DONE"
+
+# Now compare the result to be sure of your code catches all the exceptions
+for i in range(1,len(shots)+1):
+    if (shots[i-1].receivers.data == shots_pickle[i-1].receivers.data).all():
+        print "Test for receivers %d data of pickle : OK" % i 
+    else:
+        print("Test for receivers %d data of pickle : fail") % i 
+    if (shots[i-1].receivers.data == shots_savemat[i-1].receivers.data).all():
+        print "Test for receivers %d data of savemat : OK" % i 
+    else:
+        print("Test for receivers %d data of savemat : fail") % i 
+    if (shots[i-1].receivers.data == shots_h5py[i-1].receivers.data).all():
+        print "Test for receivers %d data of hdf5 : OK" % i 
+    else:
+        print("Test for receivers %d data of hdf5 : fail") % i 
+    if (shots[i-1].receivers.ts == shots_pickle[i-1].receivers.ts).all():
+        print "Test for receivers %d ts of pickle : OK" % i 
+    else:
+        print("Test for receivers %d ts of pickle : fail") % i
+    if (shots[i-1].receivers.ts == shots_savemat[i-1].receivers.ts).all():
+        print "Test for receivers %d ts of savemat : OK" % i 
+    else:
+        print("Test for receivers %d ts of savemat : fail") % i 
+    if (shots[i-1].receivers.ts == shots_h5py[i-1].receivers.ts).all():
+        print "Test for receivers %d ts of hdf5 : OK" % i 
+    else:
+        print("Test for receivers %d ts of hdf5 : fail") % i 
+
+#########################################################
+# raise some error to verify that there are well caught #
+#########################################################
+# os.remove("./shots/shot_2.hdf5")
+# generate_seismic_data_from_file(shots_h5py, save_method='h5py')
+# generate_seismic_data_from_file(shots_h5py, save_method='pickle')
+# generate_seismic_data_from_file(shots_h5py, save_method='petsc')
+# generate_seismic_data_from_file(shots_h5py)
+
+
+##############################################################
+#    Now Frequencies we save direct fourier transform data   #
+##############################################################
+solver = ConstantDensityHelmholtz(m)
+frequencies = [2.0, 3.5, 5.0, 6.5, 8.0, 9.5]
+
+# Generate synthetic Seismic data
+base_model = solver.ModelParameters(m,{'C': C})
+tt = time.time()
+
+print "shot FREQUENCY generation..."
+generate_seismic_data(shots, solver, base_model, frequencies=frequencies)
+print "DONE"
+
+print "pickle FREQUENCY generation..."
+generate_seismic_data(shots, solver, base_model,save_method='pickle', frequencies=frequencies)
+generate_seismic_data_from_file(shots_pickle, solver, save_method='pickle')
+print "DONE"
+
+print "savemat FREQUENCY generation..."
+generate_seismic_data(shots, solver, base_model,save_method='savemat', frequencies=frequencies)
+generate_seismic_data_from_file(shots_savemat, solver, save_method='savemat', frequencies=frequencies)
+print "DONE"
+
+def compare_dict(a,b):
+  if a.keys() == b.keys():
+    same = True
+    for key in a:
+      same = (same and ((a[key]-b[key]) < np.finfo(float).eps).all())
+      if same == False : break
+    return same
+  else:
+    return False
+
+
+# Now compare the result to be sure of your code catches all the exceptions
+for i in range(1,len(shots)+1):
+    if compare_dict(shots[i-1].receivers.data_dft, shots_pickle[i-1].receivers.data_dft):
+      print "Test for receivers %d data_dft of pickle : OK" % i 
+    else:
+      print("Test for receivers %d data_dft of pickle : fail") % i 
+    if compare_dict(shots[i-1].receivers.data_dft, shots_savemat[i-1].receivers.data_dft):
+      print "Test for receivers %d data_dft of savemat : OK" % i 
+    else:
+      print("Test for receivers %d data_dft of savemat : fail") % i 
+    
--- pysit/modeling/data_modeling.py
+++ pysit/modeling/data_modeling.py
@@ -4,11 +4,17 @@ import time
 
 import numpy as np
 
-__all__ = ['generate_seismic_data', 'generate_shot_data_time', 'generate_shot_data_frequency']
+import shutil #folder deletion
+
+from scipy.interpolate import interp1d
+
+import os
+
+__all__ = ['generate_seismic_data', 'generate_seismic_data_from_file', 'generate_shot_data_time', 'generate_shot_data_frequency']
 __docformat__ = "restructuredtext en"
 
 
-def generate_seismic_data(shots, solver, model, verbose=False, frequencies=None, **kwargs):
+def generate_seismic_data(shots, solver, model, verbose=False, frequencies=None, save_method=None, **kwargs):
     """Given a list of shots and a solver, generates seismic data.
 
     Parameters
@@ -25,26 +31,306 @@ def generate_seismic_data(shots, solver, model, verbose=False, frequencies=None,
     `kwargs` may be used to specify `C0` and `wavefields` to    `generate_shot_data`.
 
     """
-
+    
     if verbose:
         print('Generating data...')
         tt = time.time()
 
-    for shot in shots:
-
-        if solver.supports['equation_dynamics'] == "time":
+    if solver.supports['equation_dynamics'] == "time":
+        for shot in shots:
             generate_shot_data_time(shot, solver, model, verbose=verbose, **kwargs)
-        elif solver.supports['equation_dynamics'] == "frequency":
-            if frequencies is None:
-                raise TypeError('A frequency solver is passed, but no frequencies are given')
-            generate_shot_data_frequency(shot, solver, model, frequencies, verbose=verbose, **kwargs)
+    elif solver.supports['equation_dynamics'] == "frequency":
+        if frequencies is None:
+            raise TypeError('A frequency solver is passed, but no frequencies are given')
+        elif 'petsc' in kwargs and kwargs['petsc'] is not None:
+            # solve the Helmholtz operator for several rhs                
+            generate_shot_data_frequency_list(shots, solver, model, frequencies, verbose=verbose, **kwargs)
         else:
-            raise TypeError("A time or frequency solver must be specified.")
+            for shot in shots:            
+                generate_shot_data_frequency(shot, solver, model, frequencies, verbose=verbose, **kwargs)
+    else:
+        raise TypeError("A time or frequency solver must be specified.")
 
     if verbose:
         data_tt = time.time() - tt
         print 'Data generation: {0}s'.format(data_tt)
         print 'Data generation: {0}s/shot'.format(data_tt/len(shots))
+
+    if verbose:
+        print('Saving data...')
+        tt = time.time()
+    if solver.supports['equation_dynamics'] == "time":
+        # store shots in memory for a next simulation
+        if save_method is not None:
+            if save_method=='pickle':
+                try:
+                    import cPickle
+                except ImportError:
+                    raise ImportError('cPickle is not installed please install it and try again')
+
+                newpath = r'./shots_time'
+                if os.path.exists(newpath):
+                    shutil.rmtree(newpath)
+
+                if not os.path.exists(newpath):
+                    os.makedirs(newpath)
+
+                for i in xrange(len(shots)):
+                    fshot =newpath+'/shot_'+str(i+1)+'.save'
+                    f = file(fshot, 'wb')
+                    cPickle.dump(shots[i].receivers.data,f, protocol=cPickle.HIGHEST_PROTOCOL)
+                    f.close()
+                    fts =newpath+'/ts_'+str(i+1)+'.save'
+                    f = file(fts, 'wb')
+                    cPickle.dump(shots[i].receivers.ts,f, protocol=cPickle.HIGHEST_PROTOCOL)
+                    f.close()
+            elif save_method=='savemat':
+                try:
+                    import scipy.io as io
+                except ImportError:
+                    raise ImportError('scipy.io is not installed please install it and try again')
+
+                newpath = r'./shots_time'
+                if os.path.exists(newpath):
+                    shutil.rmtree(newpath)
+
+                if not os.path.exists(newpath):
+                    os.makedirs(newpath)
+
+                for i in xrange(len(shots)):
+                    fshot =newpath+'/shot_'+str(i+1)+'.mat'
+                    io.savemat(fshot,mdict={fshot:shots[i].receivers.data})
+                    fts =newpath+'/ts_'+str(i+1)+'.mat'
+                    io.savemat(fts,mdict={fts:shots[i].receivers.ts})
+            elif save_method=='h5py':
+                try:
+                    import h5py
+                except ImportError:
+                    raise ImportError('h5py is not installed please install it and try again')
+
+                newpath = r'./shots_time'
+
+                if os.path.exists(newpath):
+                    shutil.rmtree(newpath)
+                
+                if not os.path.exists(newpath):
+                    os.makedirs(newpath)
+
+                for i in xrange(len(shots)):
+                    fshot =newpath+'/shot_'+str(i+1)+'.hdf5'
+                    f = h5py.File(fshot,"w")
+                    dts = f.create_dataset(fshot,shots[i].receivers.data.shape,shots[i].receivers.data.dtype)
+                    dts[...] = shots[i].receivers.data
+                    f.close()
+                    fts =newpath+'/ts_'+str(i+1)+'.hdf5'
+                    f = h5py.File(fts,'w')
+                    dts = f.create_dataset(fts,shots[i].receivers.ts.shape,shots[i].receivers.ts.dtype)
+                    dts[...] = shots[i].receivers.ts
+                    f.close()
+            else:
+                raise TypeError('Unknown save_method')
+    if solver.supports['equation_dynamics'] == "frequency":
+        # store shots in memory for a next simulation
+        if save_method is not None:
+            if save_method=='pickle':
+                try:
+                    import cPickle
+                except ImportError:
+                    raise ImportError('cPickle is not installed please install it and try again')
+
+                newpath = r'./shots_frequency'
+                if os.path.exists(newpath):
+                    shutil.rmtree(newpath)
+
+                if not os.path.exists(newpath):
+                    os.makedirs(newpath)
+
+                for i in xrange(len(shots)):
+                    fshot =newpath+'/shot_'+str(i+1)+'.save'
+                    f = file(fshot, 'wb')
+                    cPickle.dump(shots[i].receivers.data_dft,f, protocol=cPickle.HIGHEST_PROTOCOL)
+                    f.close()
+            elif save_method=='savemat':
+                try:
+                    import scipy.io as io
+                except ImportError:
+                    raise ImportError('scipy.io is not installed please install it and try again')
+
+                newpath = r'./shots_frequency'
+                if os.path.exists(newpath):
+                    shutil.rmtree(newpath)
+
+                if not os.path.exists(newpath):
+                    os.makedirs(newpath)
+
+                for i in xrange(len(shots)):
+                    data_frequency = shots[i].receivers.data_dft
+                    for nu in data_frequency:
+                        fshot =newpath+'/shot_'+str(i+1)+'_nu_'+str(nu)+'.mat'
+                        io.savemat(fshot,mdict={fshot:shots[i].receivers.data_dft[nu]})
+            elif save_method=='h5py':
+                raise NotImplementedError('h5py is not efficient with frequency data please use another data container')
+            else:
+                raise TypeError('Unknown save_method')
+    if verbose:
+        save_tt = time.time() - tt
+        print 'Data saving: {0}s'.format(save_tt)
+        print 'Data saving: {0}s/shot'.format(save_tt/len(shots))
+
+def generate_seismic_data_from_file(shots, solver, verbose=False, save_method=None, **kwargs):
+    if verbose:
+        print('Loading data...')
+        tt = time.time()
+
+    if save_method is not None:
+        if solver.supports['equation_dynamics'] == "time":
+            if save_method=='pickle':
+                try:
+                    import cPickle
+                except ImportError:
+                    raise ImportError('cPickle is not installed please install it and try again')
+
+                path = r'./shots_time'
+                if os.path.exists(path):
+                    try:
+                        for i in xrange(len(shots)):                
+                            filename =path+'/ts_'+str(i+1)+'.save'
+                            f = file(filename, 'rb')
+                            ts = cPickle.load(f)
+                            f.close()
+                            shots[i].receivers.clear_data(len(ts))
+                            shots[i].receivers.ts = ts
+                            filename =path+'/shot_'+str(i+1)+'.save'
+                            f = file(filename, 'rb')
+                            shots[i].receivers.data = cPickle.load(f)
+                            f.close()
+                            shots[i].receivers.interpolator = interp1d(ts, np.zeros_like(shots[i].receivers.data),
+                                                                       axis=0, kind='linear', copy=False, bounds_error=False,
+                                                                       fill_value=0.0)
+                    except IOError:
+                        raise IOError('Files are corrupted or generated with different save_method argument')
+                else:
+                    raise ImportError('There is no shot directory, please relaunch generate_seismic_data with save_method argument')
+            
+            elif save_method=='savemat':
+                try:
+                    import scipy.io as io
+                except ImportError:
+                    raise ImportError('scipy.io is not installed please install it and try again')
+
+                path = r'./shots_time'
+                if os.path.exists(path):
+                    try:
+                        for i in xrange(len(shots)):                
+                            filename = path+'/ts_'+str(i+1)+'.mat'
+                            ts = io.loadmat(filename)
+                            ts = ts[filename]
+                            ts = ts.flatten()
+                            shots[i].receivers.clear_data(len(ts))
+                            shots[i].receivers.ts = ts
+                            filename = path+'/shot_'+str(i+1)+'.mat'
+                            data = io.loadmat(filename)
+                            data = data[filename]
+                            shots[i].receivers.data = data
+                            shots[i].receivers.interpolator = interp1d(ts, np.zeros_like(shots[i].receivers.data),
+                                                                       axis=0, kind='linear', copy=False, bounds_error=False,
+                                                                       fill_value=0.0)              
+                    except IOError:
+                        raise IOError('Files are corrupted or generated with different save_method argument')
+                else:
+                    raise ImportError('There is no shot directory, please relaunch generate_seismic_data with save_method argument')
+
+            elif save_method=='h5py':
+                try:
+                    import h5py
+                except ImportError:
+                    raise ImportError('h5py is not installed please install it and try again')
+
+                path = r'./shots_time'
+                if os.path.exists(path):
+                    try:
+                        for i in xrange(len(shots)):
+                            filename =path+'/ts_'+str(i+1)+'.hdf5'
+                            f = h5py.File(filename, 'r')
+                            dts = f[filename]
+                            ts = dts[...]              
+                            f.close()
+                            shots[i].receivers.clear_data(len(ts))
+                            shots[i].receivers.ts = ts
+                            filename = path+'/shot_'+str(i+1)+'.hdf5'
+                            f = h5py.File(filename, 'r')
+                            dts = f[filename]
+                            shots[i].receivers.data = dts[...]
+                            f.close()
+                            shots[i].receivers.interpolator = interp1d(ts, np.zeros_like(shots[i].receivers.data),
+                                                                       axis=0, kind='linear', copy=False, bounds_error=False,
+                                                                       fill_value=0.0)              
+                    except IOError:
+                        raise IOError('Files are corrupted or generated with different save_method argument')
+                else:
+                    raise ImportError('There is no shot directory, please relaunch generate_seismic_data with save_method argument')        
+            else:
+                raise TypeError('Unknown save_method')
+        elif solver.supports['equation_dynamics'] == "frequency":
+            if save_method=='pickle':
+                try:
+                    import cPickle
+                except ImportError:
+                    raise ImportError('cPickle is not installed please install it and try again')
+
+                path = r'./shots_frequency'
+                if os.path.exists(path):
+                    try:
+                        for i in xrange(len(shots)):                
+                            filename =path+'/shot_'+str(i+1)+'.save'
+                            f = file(filename, 'rb')
+                            shots[i].receivers.data_dft = cPickle.load(f)
+                            f.close()
+
+                    except IOError:
+                        raise IOError('Files are corrupted or generated with different save_method argument')
+                else:
+                    raise ImportError('There is no shot directory, please relaunch generate_seismic_data with save_method argument')
+            
+            elif save_method=='savemat':
+                try:
+                    import scipy.io as io
+                except ImportError:
+                    raise ImportError('scipy.io is not installed please install it and try again')
+
+                path = r'./shots_frequency'
+                if os.path.exists(path):
+                    try:
+                        if 'frequencies' in kwargs :
+                            data_frequency = kwargs['frequencies']
+                            for i in xrange(len(shots)):
+                                shots[i].receivers.data_dft = dict()
+                                for nu in data_frequency:
+                                    fshot =path+'/shot_'+str(i+1)+'_nu_'+str(nu)+'.mat'
+                                    data = io.loadmat(fshot)
+                                    data = data[fshot]
+                                    shots[i].receivers.data_dft[nu] = data
+                        else :
+                            raise AttributeError("""No frequencies specified in generate_seismic_data_from_file arguments""")
+                    except IOError:
+                        raise IOError('Files are corrupted or generated with different save_method argument')
+                else:
+                    raise ImportError('There is no shot directory, please relaunch generate_seismic_data with save_method argument')
+
+            elif save_method=='h5py':
+                raise NotImplementedError('h5py is not efficient with frequency data please use another data container')
+        else:
+            raise AttributeError('No solver specified cannot load data')
+    else:
+        raise AttributeError('No save_method specified cannot load data')
+
+    if verbose:
+        load_tt = time.time() - tt
+        print 'Data Loading: {0}s'.format(load_tt)
+        print 'Data Loading: {0}s/shot'.format(load_tt/len(shots))
+
+
+
 
 def generate_shot_data_time(shot, solver, model, wavefields=None, wavefields_padded=None, verbose=False, **kwargs):
     """Given a shots and a solver, generates seismic data at the specified
@@ -185,3 +471,58 @@ def generate_shot_data_frequency(shot, solver, model, frequencies, verbose=False
 
             # Record the data at frequency nu
             shot.receivers.sample_data_from_array(mesh.unpad_array(uhat), nu=nu)
+
+def generate_shot_data_frequency_list(shots, solver, model, frequencies, verbose=False, **kwargs):
+        """most of this is copied from frequency_modeling.forward_model
+
+        Parameters
+        ----------
+        shots : list of pysit.Shot
+            Gives the source signal approximation for the right hand side.
+        frequencies : list of 2-tuples
+            2-tuple, first element is the frequency to use, second element the weight.
+        return_parameters : list of {'wavefield', 'simdata', 'simdata_time', 'dWaveOp'}
+
+        Returns
+        -------
+        retval : dict
+            Dictionary whose keys are return_parameters that contains the specified data.
+
+        Notes
+        -----
+        * u is used as the target field universally.  It could be velocity potential, it could be displacement, it could be pressure.
+        * uhat is used to generically refer to the DFT of u that is needed to compute the imaging condition.
+
+        """
+        #number of shot varaible
+        nshot = len(shots)
+        # Local references
+        solver.model_parameters = model
+
+        mesh = solver.mesh
+
+        # Sanitize the input
+        if not np.iterable(frequencies):
+            frequencies = [frequencies]
+
+        rhs = solver.WavefieldVector(mesh,dtype=solver.dtype)
+        # decalare a list for the Right Hand Side
+        RHS = list()
+
+        for nu in frequencies:
+            del RHS[:]
+            for k in xrange(nshot):
+                shot = shots[k]
+                source = shot.sources
+                rhs = solver.build_rhs(mesh.pad_array(source.f(nu=nu)), rhs_wavefieldvector=rhs)
+                RHS.append(rhs.data.copy())
+                
+            Uhat = solver.solve_petsc_uhat(solver, RHS, nu, **kwargs)
+            for k in xrange(nshot):
+                    shot = shots[k]
+                    uhat = Uhat[:,k]
+                    # Giving the good dimension to the array
+                    uhat = np.expand_dims(uhat, axis=1)
+                    # Record the data at frequency nu
+                    shot.receivers.sample_data_from_array(mesh.unpad_array(uhat), nu=nu)
+        
